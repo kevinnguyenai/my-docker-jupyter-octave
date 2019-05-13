1f00262b04a3d51f04c1518b30b4d94c181c03a7
kevinnguyenai <kevin.nguyen.ai@gmail.com>
2019-05-14 00:50:14 +0700

    first commit

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,5 @@
+c.NotebookApp.ip = '0.0.0.0'
+c.NotebookApp.port = 8888
+c.NotebookApp.token = '81d9c0939715bbd54bdfe88ca54110cc3f8b76f901e58fec'
+c.NotebookApp.open_browser = False
+c.NotebookApp.notebook_dir = '/opt/'
